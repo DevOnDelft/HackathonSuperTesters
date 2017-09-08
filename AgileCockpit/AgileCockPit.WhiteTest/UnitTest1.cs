2645abe09f292662e5cf0455eff9aee664c2a55f
Sashidhar <sashizen@gmail.com>
2017-09-08 22:58:48 +0200

    WhiteTest script

---
+++ AgileCockpit/AgileCockPit.WhiteTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace AgileCockPit.WhiteTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Process.Start("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe","https://navigation.agilecockpit.com/");
+        }
+    }
+}
