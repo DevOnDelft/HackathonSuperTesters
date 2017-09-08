28e07b7436b3e8e13531ad48c63595a66ff76ec4
Sashidhar <sashizen@gmail.com>
2017-09-08 21:28:29 +0200

    Update Tests

--- AgileCockpit/TestAgileCockpit/UIMap.cs
+++ AgileCockpit/TestAgileCockpit/UIMap.cs
@@ -16,6 +16,87 @@
     
     public partial class UIMap
     {
+        /// <summary>
+        /// navigateAndSelectTeam
+        /// </summary>
+        public void navigateAndSelectTeam()
+        {
+            #region Variable Declarations
+            WinWindow uIAgileCockpitAppListGWindow1 = this.UIAgileCockpitAppListGWindow.UIAgileCockpitAppListGWindow1;
+            #endregion
 
+            // Click 'Agile Cockpit - App List - Google Chrome' window
+            Mouse.Click(uIAgileCockpitAppListGWindow1, new Point(422, 111));
+
+            // Click 'Agile Cockpit - App List - Google Chrome' window
+            Mouse.Click(uIAgileCockpitAppListGWindow1, new Point(445, 225));
+        }
+
+        /// <summary>
+        /// selectTaskBoard
+        /// </summary>
+        public void selectTaskBoard()
+        {
+            #region Variable Declarations
+            WinWindow uIAgileCockpitAppListGWindow1 = this.UIAgileCockpitAppListGWindow.UIAgileCockpitAppListGWindow1;
+            #endregion
+
+            // Click 'Agile Cockpit - App List - Google Chrome' window
+            Mouse.Click(uIAgileCockpitAppListGWindow1, new Point(793, 403));
+        }
+
+        #region Properties
+        public UIAgileCockpitAppListGWindow UIAgileCockpitAppListGWindow
+        {
+            get
+            {
+                if ((this.mUIAgileCockpitAppListGWindow == null))
+                {
+                    this.mUIAgileCockpitAppListGWindow = new UIAgileCockpitAppListGWindow();
+                }
+                return this.mUIAgileCockpitAppListGWindow;
+            }
+        }
+        #endregion
+
+        #region Fields
+        private UIAgileCockpitAppListGWindow mUIAgileCockpitAppListGWindow;
+        #endregion
+    }
+
+    [GeneratedCode("Coded UITest Builder", "14.0.23107.0")]
+    public class UIAgileCockpitAppListGWindow : WinWindow
+    {
+
+        public UIAgileCockpitAppListGWindow()
+        {
+            #region Search Criteria
+            this.SearchProperties[WinWindow.PropertyNames.Name] = "Agile Cockpit - App List - Google Chrome";
+            this.SearchProperties[WinWindow.PropertyNames.ClassName] = "Chrome_WidgetWin_1";
+            this.WindowTitles.Add("Agile Cockpit - App List - Google Chrome");
+            #endregion
+        }
+
+        #region Properties
+        public WinWindow UIAgileCockpitAppListGWindow1
+        {
+            get
+            {
+                if ((this.mUIAgileCockpitAppListGWindow1 == null))
+                {
+                    this.mUIAgileCockpitAppListGWindow1 = new WinWindow(this);
+                    #region Search Criteria
+                    this.mUIAgileCockpitAppListGWindow1.SearchProperties[WinWindow.PropertyNames.Name] = "Agile Cockpit - App List";
+                    this.mUIAgileCockpitAppListGWindow1.WindowTitles.Add("Agile Cockpit - App List - Google Chrome");
+                    #endregion
+                }
+                return this.mUIAgileCockpitAppListGWindow1;
+            }
+        }
+        #endregion
+
+        #region Fields
+        private WinWindow mUIAgileCockpitAppListGWindow1;
+        #endregion
     }
 }
